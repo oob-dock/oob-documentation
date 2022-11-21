00f15aeff17c7e1fd17eeca59e8ff9a3fc63307a
Norio Morinaga <norio.morinaga@opus-software.com.br>
2022-11-21 13:40:05 -0300

    OOFB-56 Corrigindo contagem de clientes unicos para estoque de consentimentos - informação consolidada

--- ferramentas-auxiliares/retirada-de-informaçoes-sobre-os-consentimentos/attachments/consent_function_consolidated_stock.sql
+++ ferramentas-auxiliares/retirada-de-informaçoes-sobre-os-consentimentos/attachments/consent_function_consolidated_stock.sql
@@ -5,8 +5,12 @@ CREATE OR REPLACE FUNCTION consent_consolidated_stock()
 )
 LANGUAGE SQL
 AS $$
-SELECT  SUM(CASE WHEN c.sha_business_document_number IS NULL THEN 1 ELSE 0 END) AS Clientes_Unicos_PF_Total,
-        SUM(CASE WHEN c.sha_business_document_number IS NOT NULL THEN 1 ELSE 0 END) AS Clientes_Unicos_PJ_Total
-FROM   consent c
-WHERE status = 1 
-AND   tp_consent = 1$$;
+select  SUM(CASE WHEN tb.sha_business_document_number IS NULL THEN 1 ELSE 0 END) AS Clientes_Unicos_PF_Total,
+		SUM(CASE WHEN tb.sha_business_document_number IS NOT NULL THEN 1 ELSE 0 END) AS Clientes_Unicos_PJ_Total
+from (
+	SELECT distinct c.sha_business_document_number,
+	c.sha_person_document_number 
+	FROM   consent c
+	WHERE status = 1 
+	AND   tp_consent = 1
+) tb$$;
