492141fc2be60f645648e5f6327bfac89a58efd7
Ian Ferreira Costa <ian.costa@opus-software.com.br>
2023-08-25 14:25:57 -0300

    OOFB-1709 Atualiza query de consulta de status

--- ferramentas-auxiliares/relatorio-de-requisitos-nao-funcionais/attachments/status_function_availability.sql
+++ ferramentas-auxiliares/relatorio-de-requisitos-nao-funcionais/attachments/status_function_availability.sql
@@ -1,4 +1,5 @@
-CREATE OR REPLACE FUNCTION public.status_function_availability (dt_end DATE) RETURNS TABLE (
+CREATE OR REPLACE FUNCTION status_function_availability (dt_end DATE) RETURNS TABLE (
+  metodo_http TEXT,
   endpoint varchar(200),
   data_metrica TEXT,
   periodo TEXT,
@@ -10,10 +11,11 @@ BEGIN
   RETURN QUERY
   WITH availability AS
   (
-  	SELECT
+  	select
+  	split_part(substring(e.endpoint_name , '[_-](GET|POST|PATCH|DELETE)'), '-', 1) as metodo_http,
     e.endpoint_url as endpoint,
     metric_date,
-    seconds_downtime    	
+    seconds_downtime
 	 FROM
 	    daily_metric_endpoint d
 	 INNER JOIN endpoint e on e.id = d.id_endpoint
@@ -22,21 +24,24 @@ BEGIN
 	 AND metric_date <= dt_end
   )
   (
-  	SELECT
+  	select
+     a.metodo_http,
      a.endpoint,
      to_char(dt_end - interval '89 days', 'YYYY-MM-DD') || ' - ' || to_char( dt_end, 'YYYY-MM-DD') as data_metrica,
      'Últimos três meses' AS periodo,
      (((7776000 - sum(a.seconds_downtime)) / 7776000.00) * 100)::numeric(5, 2) AS percentual_disponibilidade
 	 FROM
 	    availability a
-	 GROUP BY
+	 GROUP by
+	   a.metodo_http,
 	   a.endpoint,
        data_metrica,
 	   periodo
   )
   UNION ALL
   (
-  	SELECT
+  	select
+  	a.metodo_http,
     a.endpoint,
     to_char( metric_date, 'YYYY-MM-DD') as data_metrica,
     CASE
@@ -52,7 +57,8 @@ BEGIN
 	 FROM
 	    availability a
 	 WHERE metric_date >= dt_end - interval '6 days'
-	 GROUP BY
+	 GROUP by
+       a.metodo_http,
 	   a.endpoint,
 	   periodo,
 	   metric_date,
@@ -61,7 +67,8 @@ BEGIN
   )
   UNION ALL
   (
-  	SELECT
+  	select
+  	'POST',
     '/auth/auth/token',
     to_char( dt::date, 'YYYY-MM-DD') as data_metrica,
     CASE
