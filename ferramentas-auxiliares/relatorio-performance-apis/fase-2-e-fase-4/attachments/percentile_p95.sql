522d86f92e9ef2cd2618833e03a01c4866165804
Ian Ferreira Costa <ian.costa@opus-software.com.br>
2024-07-16 16:36:09 -0300

    OOFB-3862 Ajuste na query de P95

--- ferramentas-auxiliares/relatorio-performance-apis/fase-2-e-fase-4/attachments/percentile_p95.sql
+++ ferramentas-auxiliares/relatorio-performance-apis/fase-2-e-fase-4/attachments/percentile_p95.sql
@@ -9,40 +9,34 @@ returns table (
 language plpgsql
 as $function$
 declare
-    dt_start_utc timestamptz;
+    dt_start_utc varchar;
     dt_end_interval date;
-    dt_end_utc timestamptz;
+    dt_end_utc varchar;
     dt_start_pattern text;
 begin
     select dt_end + INTERVAL '1 day' INTO dt_end_interval;
-    select dt_start::date::timestamp AT TIME ZONE 'UTC' INTO dt_start_utc;
-    select dt_end_interval::date::timestamp AT TIME ZONE 'UTC' INTO dt_end_utc;
+    select to_char(dt_start::timestamp + interval '3 hour', 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') INTO dt_start_utc;
+    select to_char(dt_end_interval::timestamp + interval '3 hour', 'YYYY-MM-DD"T"HH24:MI:SS.MS"Z"') INTO dt_end_utc;
 
     return query
         with endpoint_list as (
             select
                 case
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%/consents/v%/consents/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/consents/.*', 'v\1/consents/{consentId}'))
+                    WHEN r.event_data#>>'{endpoint}' LIKE '%/consents/v%/consents/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/consents/[^/]+', 'v\1/consents/{consentId}'))
                     WHEN r.event_data#>>'{endpoint}' LIKE '%/accounts/%/bills/%/transactions' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/accounts/.*/bills/.*/transactions', '/accounts/{creditCardAccountId}/bills/{billId}/transactions'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%credit-cards-accounts/v%/accounts/%/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/accounts/.*/', '/accounts/{creditCardAccountId}/'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%credit-cards-accounts/v%/accounts/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/accounts/.*', '/accounts/{creditCardAccountId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%bank-fixed-incomes/v%/investments/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/investments/.*', '/investments/{investmentId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%credit-fixed-incomes/v%/investments/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/investments/.*', '/investments/{investmentId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%funds/v%/investments/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/investments/.*', '/investments/{investmentId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%treasure-titles/v%/investments/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/investments/.*', '/investments/{investmentId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%variable-incomes/v%/investments/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/investments/.*', '/investments/{investmentId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%/v%/accounts/%/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/accounts/.*/(.*)', 'v\1/accounts/{accountId}/\2'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%/v%/accounts/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/accounts/.*', 'v\1/accounts/{accountId}'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%/contracts/%/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/contracts/.*/', '/contracts/{contractId}/'))
-                    WHEN r.event_data#>>'{endpoint}' LIKE '%/contracts/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/contracts/.*', '/contracts/{contractId}'))
+                    WHEN r.event_data#>>'{endpoint}' LIKE '%credit-cards-accounts/v%/accounts/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/accounts/[^/]+', 'v\1/accounts/{creditCardAccountId}'))
+                    WHEN r.event_data#>>'{endpoint}' LIKE '%/investments/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', '/investments/[^/]+', '/investments/{investmentId}'))
+                    WHEN r.event_data#>>'{endpoint}' LIKE '%accounts/v%/accounts/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/accounts/[^/]+', 'v\1/accounts/{accountId}'))
+                    WHEN r.event_data#>>'{endpoint}' LIKE '%/contracts/%' then (REGEXP_REPLACE(r.event_data#>>'{endpoint}', 'v(.*)/contracts/[^/]+', 'v\1/contracts/{contractId}'))
                     else (r.event_data#>>'{endpoint}')
-                end                                                             as endpoint_uri,
-                (r.created_at AT TIME ZONE 'UTC' AT TIME ZONE 'UTC-3')::date    as metric_date,
-                r.event_data#>>'{httpMethod}'                                   as http_method,
-                r.brand_id                                                      as brand_id,
-                r.event_data#>>'{processTimespan}'                              as processTimespan
+            end 			                                      			as endpoint_uri,
+                -- using timestamp to avoid error due to database timezone configuration
+                (r.event_timestamp::timestamp - interval '3 hour')::date    as metric_date,
+                r.event_data#>>'{httpMethod}' 					  			as http_method,
+                r.brand_id										  			as brand_id,
+                r.event_data#>>'{processTimespan}'				  			as processTimespan
             from report r
-                where r.created_at between dt_start_utc and dt_end_utc and r.event_role = 'SERVER'
+                where r.event_timestamp between dt_start_utc and dt_end_utc and r.event_role = 'SERVER'
                 and r.event_data#>>'{endpoint}' not like '/open-banking/payments%'
                 and r.event_data#>>'{endpoint}' not like '/open-banking/automatic-payments%'
                 and r.event_data#>>'{endpoint}' not like '/open-banking/opendata%'
