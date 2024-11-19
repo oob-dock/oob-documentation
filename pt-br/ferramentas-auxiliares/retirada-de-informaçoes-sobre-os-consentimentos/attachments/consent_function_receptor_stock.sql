cdb5520d0d07cc5971f8ea68016a575c0fb474e1
Norio Morinaga <norio.morinaga@opus-software.com.br>
2024-11-19 13:53:01 -0300

    OOFB-5157 Updating queries

--- pt-br/ferramentas-auxiliares/retirada-de-informaçoes-sobre-os-consentimentos/attachments/consent_function_receptor_stock.sql
+++ pt-br/ferramentas-auxiliares/retirada-de-informaçoes-sobre-os-consentimentos/attachments/consent_function_receptor_stock.sql
@@ -20,7 +20,7 @@ BEGIN
 	    from consent c
 	        inner join tpp on tpp.id = c.id_tpp
 	        inner join history_status hs on hs.id_consent = c.id
-	    where c.id = any(consent_uuids) and c.tp_consent = 1 and c.dt_creation <= dt_end_utc and c.dt_expiration >= dt_end_utc and c.status in (1,4,5)
+	    where c.id = any(consent_uuids) and c.tp_consent = 1 and c.dt_creation <= dt_end_utc and (c.dt_expiration is null or c.dt_expiration >= dt_end_utc) and c.status in (1,4,5)
 	        and exists (select 1 from history_status hs where hs.id_consent = c.id and hs.status_consent = 1)
 	        and not exists (select 1 from history_status hs2 where hs2.id_consent = c.id and hs2.status_consent = 4 and hs2.updated_on <= dt_end_utc)
 	        group by tpp.org_name, tpp.org_id;
