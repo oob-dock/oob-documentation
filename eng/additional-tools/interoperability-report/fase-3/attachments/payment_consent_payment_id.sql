00eb274cc00b3972c7e77110530739d647f29ed2
Norio Morinaga <norio.morinaga@opus-software.com.br>
2025-02-14 16:13:25 -0300

    OOFB-6120 Fixing queries

--- eng/additional-tools/interoperability-report/fase-3/attachments/payment_consent_payment_id.sql
+++ eng/additional-tools/interoperability-report/fase-3/attachments/payment_consent_payment_id.sql
@@ -20,23 +20,25 @@ BEGIN
     SELECT end_date_interval::date::timestamp AT TIME ZONE 'UTC' into end_date_utc;
 
                 RETURN QUERY
-                  SELECT  t.org_name::TEXT                                           		 itp,
-	                      count(distinct pr.id)                                      	     quantity_request,
-                          count(distinct p.id)										         quantity_payment_id,
-                          count(distinct p.id) FILTER (where p.payment_status = 7)           quantity_completed_payment_id,
-                          count(distinct p.id) FILTER (where p.payment_status = 9)           quantity_schedule_payment_id,
+                  SELECT  t.org_name::TEXT                                          itp,
+	                      count(distinct pr.id)                                      	quantity_request,
+                          count(distinct p.id)										                  quantity_payment_id,
+                          count(distinct p.id) FILTER (where p.payment_status = 7)  quantity_completed_payment_id,
+                          count(distinct p.id) FILTER (where p.payment_status = 9)  quantity_schedule_payment_id,
                           case 
-                          	WHEN c.tp_modality_payment = 1 THEN 'IMMEDIATE'
-			                WHEN c.tp_modality_payment = 2 THEN 'SCHEDULED'
-			                ELSE 'RECURRENT'
-			              END AS product,
-                          COALESCE(elem->>'authorisationFlow', 'HYBRID_FLOW') AS authorisation_flow
+                            WHEN c.tp_modality_payment = 1 THEN 'IMMEDIATE'
+                            WHEN c.tp_modality_payment = 2 THEN 'SCHEDULED'
+                            ELSE 'RECURRENT'
+                          END AS                                                    product,
+                          CASE
+                            WHEN c.id_enrollment IS NOT NULL THEN 'FIDO_FLOW'
+                            ELSE 'HYBRID_FLOW'
+                          END AS                                                    authorisation_flow
                   FROM consent c
                   LEFT JOIN tpp t ON c.id_tpp = t.id  
                   LEFT JOIN payment_request pr ON c.id = pr.id_consent  
                   LEFT JOIN payment p ON pr.id = p.id_payment_request  
-                  LEFT JOIN LATERAL jsonb_array_elements(pr.payment_payload_data->'data') AS elem ON true
-                 WHERE ((c.dt_creation BETWEEN start_date_utc AND end_date_utc) OR (pr.dt_payment_request BETWEEN start_date_utc AND end_date_utc))
+                 WHERE (((c.dt_creation BETWEEN start_date_utc AND end_date_utc) and C.status in (4,5,6)) OR (pr.dt_payment_request BETWEEN start_date_utc AND end_date_utc))
                     AND c.tp_consent = 2
                     AND ((is_automatic is false AND c.tp_modality_payment IN (1,2)) OR (is_automatic is true AND c.tp_modality_payment IN (3,4,5)))
                   GROUP BY t.org_name, c.tp_modality_payment, authorisation_flow
--- pt-br/ferramentas-auxiliares/relatorio-de-interoperabilidade/fase-3/attachments/payment_consent_function_count.sql
+++ pt-br/ferramentas-auxiliares/relatorio-de-interoperabilidade/fase-3/attachments/payment_consent_function_count.sql
@@ -22,13 +22,18 @@ BEGIN
                     count(c.id)                                                                             quantity_request,
                     count(c.id) FILTER(WHERE c.account_holder_status=1  or c.account_holder_status is null) quantity_consent_client,
                     count(c.id) FILTER(WHERE c.account_holder_status=2)                                     quantity_consent_non_client,
-                    t.org_id::TEXT                                                                          itp_org_id
+                    t.org_id::TEXT                                                                          itp_org_id,
+                    case
+                      WHEN c.tp_modality_payment = 1 THEN 'IMMEDIATE'
+                      WHEN c.tp_modality_payment = 2 THEN 'SCHEDULED'
+                      ELSE 'RECURRENT'
+                    END AS product
                   FROM consent c
                     INNER JOIN tpp t ON c.id_tpp = t.id
                   WHERE c.dt_creation BETWEEN start_date_utc AND end_date_utc
                     AND c.tp_consent = 2
                     AND ((is_automatic is false AND c.tp_modality_payment IN (1,2)) OR (is_automatic is true AND c.tp_modality_payment IN (3,4,5)))
-                  GROUP BY t.org_name, t.org_id
+                  GROUP BY t.org_name, t.org_id, c.tp_modality_payment
                   ORDER BY t.org_name ASC, quantity_request DESC;
 END;
 $function$ LANGUAGE plpgsql;
